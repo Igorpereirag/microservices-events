d500aa6903d560b104122ebde746057c3ea39bf5
IGOR <igorpereira1501@gmail.com>
2023-12-22 12:23:17 -0300

    subscription repository implementado

---
+++ src/main/java/Repository/EventRepository.java
@@ -0,0 +1,23 @@
+package Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.microservices.microservicesevents.domain.Event;
+
+import jakarta.annotation.Nonnull;
+
+public interface EventRepository extends JpaRepository<Event, String> {
+    
+    @Query(value = "SELECT * FROM events e WHERE parsedatetime(e.date, 'dd/MM/yyyy') > :currentDate", nativeQuery = true)
+    List<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate);
+
+	@Nonnull
+    Optional<Event> findById(@Nonnull String id);
+    
+}
---
+++ src/main/java/Repository/SubscriptionRepository.java
@@ -0,0 +1,12 @@
+package Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservices.microservicesevents.domain.Subscription;
+
+import jakarta.persistence.Id;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Id> {
+    
+    
+}
